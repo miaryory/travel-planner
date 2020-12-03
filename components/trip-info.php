01e45162dea65f389e4c7668221ff3334e772674
Miary <miar0004@stud.kea.dk>
2020-12-04 00:53:24 +0100

    subpages added

---
+++ components/trip-info.php
@@ -0,0 +1,3 @@
+<div class="trip-info">
+    
+</div>
---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Planner</title>
+    <link rel="stylesheet" href="css/mobile-style.css">
+    <link rel="stylesheet" href="css/tablet-style.css">
+    <link rel="stylesheet" href="css/desktop-style.css">
+</head>
+<body id="dashboard-page">
+
+    <?php require_once(__DIR__.'/components/navbar.php') ?>
+    <?php require_once(__DIR__.'/components/all-trips.php') ?>
+    <?php require_once(__DIR__.'/components/trip-details.php') ?>
+
+    
+</body>
+</html>
